953c3d4e2e281b820663040291c2fb350117cb42
DipoAreoye <dipoareoye@gmail.com>
2024-11-20 17:14:30 -0500

    nextJS

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/assetlinks.json',
+        destination: '/api/.well-known/assetlinks',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
